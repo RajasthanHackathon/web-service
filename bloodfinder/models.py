1ca53eed1f51ecc13ae0c1d70f4dbc8830c02787
ParadoxZero <sidhin.thomas@gmail.com>
2018-03-19 23:56:50 +0530

    models

--- bloodfinder/models.py
+++ bloodfinder/models.py
@@ -2,3 +2,97 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+
+class BloodGroups:
+    Ap = 'A+'
+    An = 'A-'
+    Bp = 'B+'
+    Bn = 'B-'
+    ABp = 'AB+'
+    ABn = 'AB-'
+    Op = 'O+'
+    On = 'O-'
+    CHOICES = (
+        (Ap, Ap),
+        (An, An),
+        (Bp, Bp),
+        (Bn, Bn),
+        (Op, Op),
+        (On, On),
+        (ABp, ABp),
+        (ABn, ABn)
+    )
+
+
+class Donor(models.Model):
+    name = models.CharField(max_length=250)
+    phone = models.CharField(max_length=10, unique=True)
+    blood_group = models.CharField(max_length=4, choices=BloodGroups.CHOICES)
+    district = models.CharField(max_length=2)
+    bhamasha_id = models.CharField(max_length=15)
+    family_id = models.CharField(max_length=15)
+    aadhar_number = models.CharField(max_length=20)
+
+
+class PhoneNumber(models.Model):
+    """
+    For fraud protection, normalize the phone number.
+    """
+    phone = models.CharField(max_length=10, unique=True)
+
+
+class Request(models.Model):
+    phone = models.ForeignKey(PhoneNumber, on_delete=models.SET_NULL, null=True)
+    blood_group = models.CharField(max_length=4, choices=BloodGroups.CHOICES)
+    high_volume = models.BooleanField(default=False)
+    district = models.CharField(max_length=2)
+
+    time = models.DateTimeField(default=timezone.now)
+    is_completed = models.BooleanField(default=False)
+    is_fraudulent = models.BooleanField(default=False)
+
+    rejected_list = models.ManyToManyField(Donor)
+
+    is_verified = models.BooleanField(default=False)
+
+
+class Donations(models.Model):
+    donor = models.ForeignKey(Donor, on_delete=models.SET_NULL, null=True)
+    request = models.ForeignKey(Request, on_delete=models.SET_NULL, null=True)
+
+    is_completed = models.BooleanField(default=False)
+    is_accepted = models.NullBooleanField()
+
+
+class Complaints(models.Model):
+    phone = models.ForeignKey(PhoneNumber, on_delete=models.SET_NULL, null=True)
+    donor = models.ForeignKey(Donor, on_delete=models.SET_NULL, null=True)
+    complaint_by_donor = models.BooleanField(default=False)
+    text = models.TextField()
+    investigating = models.BooleanField(default=False)
+    resolved = models.BooleanField(default=False)
+    rejected = models.NullBooleanField()
+
+
+class SMSBuffer(models.Model):
+    """
+    Not a core model - specific to prototype to simulate SMS transaction
+    """
+    sender = models.CharField(max_length=10)
+    to = models.CharField(max_length=10)
+    message = models.TextField()
+
+    is_sent = models.BooleanField(default=False)
+
+    @property
+    def serialize(self):
+        return {
+            'from': self.sender,
+            'to': self.to,
+            'text': self.message
+}
